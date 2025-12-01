485f911fe90e45c676bbba7d232b51cb97423a88
dooripapa <b532882@hotmail.com>
2025-12-01 20:36:12 +0900

    Quekka, Publisher, Subscriber pseudo코드 추가.

---
+++ src/unittest/test_publisher.cpp
@@ -0,0 +1,39 @@
+//
+// Created by b532882 on 12/1/25.
+//
+#include <gtest/gtest.h>
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <tuple>
+#include <future>
+#include <thread>
+
+// ------------------ 테스트 예시 ------------------
+
+using namespace std;
+
+TEST(publisher, usage) {
+    Publisher publisher;
+
+    string ip = "127.0.0.1";
+    string port = "8080";
+
+    Quekka quekka{ip, port};
+    int status = quekka.connect();
+    if (status == -1) {
+        printf ("Can't connect to quekka server\n");
+    }
+
+    string topic = "hello";
+    int CanConn? = publisher.greet (quekka, topic);
+    if (CanConn) {
+        cout << "success" << endl;
+    }
+
+    while (1) {
+        sleep(1);
+        publisher.publish("it's me, publisher");
+    }
+}
